a66e851bb881af696201f70823e4c56b0b35f31c
ValeriaDelvi <byvale@live.it>
2024-03-15 13:01:16 +0100

    React tutorial, pink buttons

---
+++ react-tutorial/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+   <div className="flex justify-center items-center">
+    <button className="bg-pink-500 text-white  p-3 rounded-lg m-3" >
+e svegliarsi
+    </button>
+
+    <button className="bg-pink-600 text-white   p-3 rounded-lg m-3" >
+la mattina
+    </button>
+
+    <button className="bg-pink-700 text-white   p-3 rounded-lg m-3" >
+tu turu turu TUTTUUUUUUU
+    </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-tutorial/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'; //react è variabile possiamo usare un nome qualunque tanto importiamo tutto di default
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
